09c9aa20c924493cc6a70807f3936accac45542a
Weerawat Fo <weerawat190943@gmail.com>
2021-04-24 00:13:52 +0700

    edit setting

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDAIA_URL = "/images/"
+STACTIFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
