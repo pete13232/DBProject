6c10441cb048008368f89635b5a1bf58e7c61ce9
Weerawat Fo <weerawat190943@gmail.com>
2021-04-29 01:36:07 +0700

    "fix"

--- myproject/app/urls.py
+++ myproject/app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     # path("", views.card3Col, name="card3Col"),
-    path("test", views.test, name="test"),
+    path("", views.test, name="test"),
     path("resCard", views.resCard, name="resCard"),
 ]
