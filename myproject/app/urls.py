86ea42d37ed2b6a51fc7cdf279b8d1249c3e82c5
Weerawat Fo <weerawat190943@gmail.com>
2021-05-29 22:14:15 +0700

    review created

--- myproject/app/urls.py
+++ myproject/app/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 urlpatterns = [
     # path("", views.card3Col, name="card3Col"),
-    path("", views.test, name="test"),
+    path("", views.index, name="index"),
     path("resCard", views.resCard, name="resCard"),
     path("card3Col", views.card3Col, name="card3Col"),
     path("categoryCard", views.categoryCard, name="categoryCard"),
     path("login", views.login, name="login"),
     path("signup", views.signup, name="signup"),
+    path("review", views.review, name="review"),
     
 ]
