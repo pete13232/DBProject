957256c58edede0d168ba157e04ad4c83a109e11
Weerawat Fo <weerawat190943@gmail.com>
2021-05-30 22:54:21 +0700

    page first trial

--- myproject/app/views.py
+++ myproject/app/views.py
@@ -6,24 +6,29 @@ def card3Col(request):
 
 
 def index(request):
-
-    return render(request, "index.html")
+    myContext = {
+        "titles": ["A", "B", "C", "D", "E", "F"]
+        }
+    return render(request, "index.html", myContext)
 
 
 def resCard(request):
 
     return render(request, "resCard.html")
 
+
 def categoryCard(request):
 
     return render(request, "categoryCard.html")
 
+
 def login(request):
     return render(request, "login.html")
+
 
 def signup(request):
     return render(request, "signup.html")
 
+
 def review(request):
     return render(request, "review.html")
-
