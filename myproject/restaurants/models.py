a433991ea8a5475456c96073140aaf47fdaaa832
Weerawat Fo <weerawat190943@gmail.com>
2021-06-07 22:13:32 +0700

    ร้อนนน

--- myproject/restaurants/models.py
+++ myproject/restaurants/models.py
@@ -47,6 +47,19 @@ class Company(models.Model):
             + "&color=7F9CF5&background=EBF4FF"
         )
 
+    def fullAddress(self):
+        return (
+            self.address
+            + " "
+            + self.subDistrict
+            + " "
+            + self.district
+            + " "
+            + self.province
+            + " "
+            + self.postalCode
+        )
+
     def __str__(self):
         return self.companyID
 
